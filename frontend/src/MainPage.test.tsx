02489fe19d97784590391f6b1c7f23fb3b809c16
guboc11 <guboc11@gmail.com>
2024-02-04 12:57:07 +0900

    Add NavigationBar exist test to MainPage & Fix it

--- frontend/src/MainPage.test.tsx
+++ frontend/src/MainPage.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import MainPage from './MainPage';
 
-test("메인페이지가 존재함",()=>{
+test("MainPage가 존재함",()=>{
   render(<MainPage/>)
   const mainpage = screen.getByTitle("MainPage")
   expect(mainpage).toBeInTheDocument();
+})
+
+test("NavigationBar가 MainPage에 존재한다.", ()=>{
+  render(<MainPage/>)
+  const navigationbar = screen.getByTitle("NavigationBar")
+  expect(navigationbar).toBeInTheDocument();
 })
--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,9 +1,11 @@
 import { Box, Typography } from "@mui/material"
+import NavigationBar from "./NavigationBar"
 
 export default function MainPage() {
   return(
     <Box title="MainPage">
       <Typography>Main Page</Typography>
+      <NavigationBar></NavigationBar>
     </Box>
   )
 }
