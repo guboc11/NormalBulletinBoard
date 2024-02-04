ee478d1b58be7667916e7a403bb443e223da13a3
guboc11 <guboc11@gmail.com>
2024-02-04 12:53:56 +0900

    Fix NavigationBar test

--- frontend/src/NavigationBar.test.tsx
+++ frontend/src/NavigationBar.test.tsx
@@ -1,3 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import NavigationBar from './NavigationBar';
 
-test("NavigationBar가 존재한다.")
+test("NavigationBar가 존재한다.", ()=>{
+  render(<NavigationBar/>)
+  const navigationbar = screen.getByTitle("NavigationBar")
+  expect(navigationbar).toBeInTheDocument();
+})
---
+++ frontend/src/NavigationBar.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material"
+
+export default function NavigationBar() {
+  return(
+    <Box title="NavigationBar">
+      <Typography>Navigation Bar</Typography>
+    </Box>
+  )
+
+}
