5754027024c31de3a538168e235fec796e7879d1
guboc11 <guboc11@gmail.com>
2024-02-04 13:25:08 +0900

    Fix 전체 보기 버튼, 글 쓰기 버튼이 존재한다. test

--- frontend/src/NavigationBar.tsx
+++ frontend/src/NavigationBar.tsx
@@ -1,9 +1,11 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 
 export default function NavigationBar() {
   return(
     <Box title="NavigationBar">
       <Typography>Navigation Bar</Typography>
+      <Button title="ViewAllButton">View All</Button>
+      <Button title="NewPostButton">New Post</Button>
     </Box>
   )
 
