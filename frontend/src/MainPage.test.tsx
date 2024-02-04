c4a9d29cdb2c143a2ec2cf462e6cfa93f4d40ffe
guboc11 <guboc11@gmail.com>
2024-02-04 12:59:14 +0900

    Add MainBody exist in MainPage test

--- frontend/src/MainPage.test.tsx
+++ frontend/src/MainPage.test.tsx
@@ -12,3 +12,9 @@ test("NavigationBar가 MainPage에 존재한다.", ()=>{
   const navigationbar = screen.getByTitle("NavigationBar")
   expect(navigationbar).toBeInTheDocument();
 })
+
+test("MainBody가 MainPage에 존재한다.", ()=>{
+  render(<MainPage/>)
+  const mainbody = screen.getByTitle("MainBody")
+  expect(mainbody).toBeInTheDocument();
+})
